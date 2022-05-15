fd07e544fca3e0d80781a1004e15624644719e4b
aawfrancois <aawfrancois@gmail.com>
2022-05-15 18:18:17 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,134 +1,49 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/router/Router.php';
+require_once __DIR__ . '/router/Route.php';
+require_once __DIR__ . '/controllers/Movies.php';
+require_once __DIR__ . '/controllers/Drivers.php';
+require_once __DIR__ . '/services/Auth.php';
 
-// In case one is using PHP 5.4's built-in server
-$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-if (php_sapi_name() === 'cli-server' && is_file($filename)) {
-    return false;
+$auth = new services\Auth();
+
+//20220515
+
+if (!$auth->checkTokenIsValid()) {
+    http_response_code(401);
+    exit();
 }
 
-// Include the Router class
-// @note: it's recommended to just use the composer autoloader when working with other packages too
-require_once __DIR__ . '/router/Router.php';
-
-// Create a Router
 $router = new router\Router();
+$route = new router\Route();
+$moviesController = new controllers\Movies();
+$driversController = new controllers\Drivers();
 
-// Custom 404 Handler
-$router->set404(function () {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    echo '404, route not found!';
-});
+if (isset($_GET['search'])) {
+    $search = $_GET['search'];
+} else {
+    $search = '';
+}
 
-// custom 404
-$router->set404('/test(/.*)?', function () {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    echo '<h1><mark>404, route not found!</mark></h1>';
-});
+if (isset($_GET['page']) === false) {
+    $route->route('/api?search=' . $search, function () use ($driversController, $search, $moviesController) {
 
-$router->set404('/api(/.*)?', function() {
-    header('HTTP/1.1 404 Not Found');
-    header('Content-Type: application/json');
-
-    $jsonArray = array();
-    $jsonArray['status'] = "404";
-    $jsonArray['status_text'] = "route not defined";
-
-    echo json_encode($jsonArray);
-});
-
-// Before Router Middleware
-$router->before('GET', '/.*', function () {
-    header('X-Powered-By: bramus/router');
-});
-
-// Static route: / (homepage)
-$router->get('/api', function () {
-    echo '<h1>bramus/router</h1>
-              <p>Try these routes:<p>
-              <ul>
-                <li><a href="/hello/joe">/hello/<em>name</em></a></li>
-                <li><a href="/blog">/blog</a></li>
-                <li><a href="/blog/'.date('Y').'">/blog/<em>year</em></a></li>
-                <li><a href="/blog/'.date('Y').'/'.date('m').'">/blog/<em>year</em>/<em>month</em></a></li>
-                <li><a href="/blog/'.date('Y').'/'.date('m').'/'.date('d').'">/blog/<em>year</em>/<em>month</em>/<em>day</em></a></li>
-                <li><a href="/movies">/movies</a></li>
-                <li><a href="/movies/23">/movies/<em>id</em></a></li>
-              </ul>
-              <br><br>
-              <p>Custom error routes</p>
-              <ul>
-                <li><a href="/something">/*</a> <em>Normal 404</em></li>
-                <li><a href="/test">/test/*</a> <em>Custom 404</em></li>
-                <li><a href="/api/getUser">/api/getUser</a> <em>API 404</em></li>
-              </ul>
-        ';
-});
-
-// Static route: /hello
-$router->get('/hello', function () {
-    echo '<h1>bramus/router</h1><p>Visit <code>/hello/<em>name</em></code> to get your Hello World mojo on!</p>';
-});
-
-// Dynamic route: /hello/name
-$router->get('/hello/(\w+)', function ($name) {
-    echo 'Hello ' . htmlentities($name);
-});
-
-// Dynamic route: /ohai/name/in/parts
-$router->get('/ohai/(.*)', function ($url) {
-    echo 'Ohai ' . htmlentities($url);
-});
-
-// Dynamic route with (successive) optional subpatterns: /blog(/year(/month(/day(/slug))))
-$router->get('/blog(/\d{4}(/\d{2}(/\d{2}(/[a-z0-9_-]+)?)?)?)?', function ($year = null, $month = null, $day = null, $slug = null) {
-    if (!$year) {
-        echo 'Blog overview';
-
-        return;
-    }
-    if (!$month) {
-        echo 'Blog year overview (' . $year . ')';
-
-        return;
-    }
-    if (!$day) {
-        echo 'Blog month overview (' . $year . '-' . $month . ')';
-
-        return;
-    }
-    if (!$slug) {
-        echo 'Blog day overview (' . $year . '-' . $month . '-' . $day . ')';
-
-        return;
-    }
-    echo 'Blogpost ' . htmlentities($slug) . ' detail (' . $year . '-' . $month . '-' . $day . ')';
-});
-
-// Subrouting
-$router->mount('/movies', function () use ($router) {
-
-    // will result in '/movies'
-    $router->get('/', function () {
-        echo 'movies overview';
+        $driversController->getDataDriverFiltred($search);
+        $moviesController->getAllDataFiltred($search);
     });
+} else {
+    $page = $_GET['page'];
 
-    // will result in '/movies'
-    $router->post('/', function () {
-        echo 'add movie';
+    $route->route('/api?search=' . $search . '&page=' . $page, function () use ($page, $search, $moviesController) {
+        $moviesController->getAllDataFiltred($search, (string) $page);
     });
+}
 
-    // will result in '/movies/id'
-    $router->get('/(\d+)', function ($id) {
-        echo 'movie id ' . htmlentities($id);
-    });
 
-    // will result in '/movies/id'
-    $router->put('/(\d+)', function ($id) {
-        echo 'Update movie id ' . htmlentities($id);
-    });
+$route->route('/404', function () {
+    echo "Page not found";
 });
 
-// Thunderbirds are go!
 $router->run();
 
-// EOF
