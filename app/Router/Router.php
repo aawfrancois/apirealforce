a008d23d535f382d10d52cbfe1f6e0b6f50fdc1f
aawfrancois <aawfrancois@gmail.com>
2022-05-17 20:14:56 +0200

    update addRoute

--- app/Router/Router.php
+++ app/Router/Router.php
@@ -8,34 +8,98 @@ namespace App\Router;
 class Router
 {
     /**
-     * @return void
+     * @var array $routes
      */
-
     public array $routes = [];
 
-    function run(): void
+    /**
+     * Run router
+     *
+     * @return $this
+     */
+    public function run(): self
     {
         $uri = $_SERVER['REQUEST_URI'];
         $found = false;
 
-        foreach ($this->routes as $path => $callback) {
-            if ($path !== $uri) continue;
+        foreach ($this->routes as $route) {
+            $parsedUrl = parse_url($uri);
+            if ($parsedUrl === false) {
+                continue;
+            }
+            if ($route['path'] !== $parsedUrl['path'] || $route['method'] !== strtolower($_SERVER['REQUEST_METHOD'])) {
+                continue;
+            }
 
             $found = true;
-            $callback();
+            $explodeCallable = explode('@', $route['callable']);
+            if (count($explodeCallable) !== 2) {
+                throw new \RuntimeException('invalid callable for the route');
+            }
+
+            $controller = new $explodeCallable[0]();
+            $action = $explodeCallable[1];
+            $controller->$action();
         }
 
         if (!$found) {
-            $notFoundCallback = $this->routes['/404'];
-            $notFoundCallback();
+            self::throw404();
         }
+
+        return $this;
     }
-    public function addRoute(string $name, string $path, callable $callable): void
+
+    /**
+     * @param string $name
+     * @param string $method
+     * @param string $path
+     * @param string $callable
+     * @return Router
+     */
+    public function addRoute(string $name, string $method, string $path, string $callable): self
     {
         $this->routes[$name] = [
             'path' => $path,
-            'callable' => $callable,
+            'method' => strtolower($method),
+            'callable' => $callable
         ];
+
+        return $this;
     }
 
+    /**
+     * @return void
+     */
+    public static function throw400()
+    {
+        http_response_code(400);
+        exit();
+    }
+
+    /**
+     * @return void
+     */
+    public static function throw401()
+    {
+        http_response_code(401);
+        exit();
+    }
+
+    /**
+     * @return void
+     */
+    public static function throw403()
+    {
+        http_response_code(403);
+        exit();
+    }
+
+    /**
+     * @return void
+     */
+    public static function throw404()
+    {
+        http_response_code(404);
+        exit();
+    }
 }
--- public/index.php
+++ public/index.php
@@ -4,39 +4,12 @@ header('Access-Control-Allow-Origin: *');
 
 $router = new \App\Router\Router();
 
-
-
-$moviesController = new controllers\Movies();
-$driversController = new controllers\Drivers();
-
-$auth = new Services\Auth();
+$auth = new \App\Services\Auth();
 if (!$auth->checkTokenIsValid()) {
-    echo '401 unauthorized';
-    http_response_code(401);
-    exit();
+    \App\Router\Router::throw401();
 }
 
-$search = $_GET['search'] ?? '';
-
-if (isset($_GET['page']) === false) {
-    $route->route('/api?search=' . $search, function () use ($driversController, $search, $moviesController) {
-
-        $driverArray = $driversController->getDataDriverFiltred($search);
-        $moviesArray = $moviesController->getAllDataFiltred($search);
-
-        echo json_encode(array_merge($driverArray, $moviesArray));
-    });
-} else {
-    $page = $_GET['page'];
-
-    $route->route('/api?search=' . $search . '&page=' . $page, function () use ($page, $search, $moviesController) {
-        $moviesController->getAllDataFiltred($search, (string) $page);
-    });
-}
-
-$route->route('/404', function () {
-    echo "Page not found";
-});
+$router->addRoute('search-get', 'get', '/search', '\App\Controllers\Movies@search');
 
 $router->run();
 
